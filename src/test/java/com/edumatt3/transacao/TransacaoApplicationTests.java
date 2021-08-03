69e27d9f7ffbec92335053ab78fe7ba5450fadf8
eduardo-zup <eduardo.bento@zup.com.br>
2021-08-03 16:20:41 -0300

    setup inicial

---
+++ src/test/java/com/edumatt3/transacao/TransacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edumatt3.transacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
